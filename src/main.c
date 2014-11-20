85e282e70eaf1974dedc7903d75bcb1eb557f91f
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-11-20 05:12:05 +0100

    Add functioning usage of screensaver kernel

--- src/main.c
+++ src/main.c
@@ -7,63 +7,64 @@
 #include "colors.h"
 
 int main(void) {
-    /* Chip errata */
-    CHIP_Init();
+	/* Chip errata */
+	CHIP_Init();
 
-    /* Init correct ebi banks */
-    ebi_setup();
+	/* Init correct ebi banks */
+	ebi_setup();
 
+	for (int i = 0; i < 4096; i++) {
+		uint16_t data = rgb(0, 0, 255);
+		if (i % 64 < 1 || i % 64 > 62 || i / 64 < 1 || i /64 > 62) {
+			data = rgb(255, 0, 0);
+		}
+		int write_address = SRAM_BASE_ADDR + (i << 1);
+		*(uint16_t*)write_address = data;
+	}
 
-    for (int i = 0; i < 1024; i++) {
-        uint16_t data = rgb(0, 0, 255);
-        int write_address = SRAM_BASE_ADDR + (i << 1);
-        *(uint16_t*)write_address = data;
-    }
+	load_kernel(1, square_kernel, 33);
+	load_kernel(101, id_kernel, 23);
+	load_kernel(201, constants, 17);
+	load_kernel(301, flip, 20);
+	load_kernel(350, srl, 17);
+	load_kernel(400, test_64_width, 19);
+	load_kernel(500, draw_square, 40);
 
-    load_kernel(1, square_kernel, 33);
-    load_kernel(101, id_kernel, 23);
-    load_kernel(201, constants, 17);
-    load_kernel(301, flip, 20);
-    load_kernel(350, srl, 17);
-    load_kernel(400, test_64_width, 19);
-    load_kernel(500, draw_square, 40);
+	/* Screensaver cube */
+	int cube_width = 20;
+	int x = 20;
+	int y = 10;
+	int xhi = x + cube_width;
+	int yhi = y + cube_width;
 
-    /* Infinite loop */
-    int cube_width = 30;
-    int x = 10;
-    int y = 10;
-    int xhi = x + cube_width;
-    int yhi = y + cube_width;
+	load_constant(0, x);
+	load_constant(1, y);
+	load_constant(2, xhi);
+	load_constant(3, yhi);
 
-    load_constant(0, x);
-    load_constant(1, y);
-    load_constant(2, xhi);
-    load_constant(3, yhi);
+	int dy = 1;
+	int dx = 1;
+	while (1) {
+		start_kernel(500, 512);
 
-    int i = 0;
-    int direction = 1;
-    while (1) {
-        start_kernel(500, 1024);
+		if (x <= 0 || x >= (63 - cube_width)) {
+			dx *= -1;
+		}
 
-        load_constant(0, x);
-        load_constant(2, xhi);
+		if (y <= 0 || y >= (63 - cube_width)) {
+			dy *= -1;
+		}
 
-        if (x <= -(cube_width/2) || x > (64 - cube_width/2)) {
-            direction *= -1;
-        }
+		x += dx;
+		y += dy;
+		xhi = x + cube_width;
+		yhi = y + cube_width;
 
-        if(x <= -(cube_width/2)) {
-            y += -direction;
-        }
+		load_constant(0, x);
+		load_constant(1, y);
+		load_constant(2, xhi);
+		load_constant(3, yhi);
 
-        if(x > (64 - cube_width/2)) {
-            y += direction;
-        }
-
-        x += direction * 2;
-        xhi = x + cube_width;
-
-        for (int i=0; i<200000; i++);
-        i++;
-    }
+		for (int i=0; i<200000; i++);
+	}
 }
